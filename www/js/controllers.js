6db46d9889aed006429d8f96b1c3c55f133bf287
Earl <earlvanze@gmail.com>
2015-08-24 17:36:19 -0400

    Attempted implementing new Messaging module from http://codepen.io/anon/pen/xGoMoZ?editors=001 but getting error 'userMessageScroll could not find a corresponding element with delegate-handle=userMessageScroll! scrollBottom() was not called! Possible cause: If you are calling scrollBottom() immediately, and your element with delegate-handle=userMessageScroll is a child of your controller, then your element may not be compiled yet. Put a  around your call to scrollBottom() and try again.'

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,9 +20,7 @@ angular.module("mealcarrier.controller", ["mealcarrier.services", "mealcarrier.f
 		    //success
 		    console.log($response);
 		    if (!$response.data.success){
-		    	if (!$response.data.success){
-		    		console.log($response.data.message)
-		    	}
+	    		console.log($response.data.message)
 		    } else {
 		        // Login was successful
 			    // We need to save the information from the login
@@ -494,51 +492,189 @@ angular.module("mealcarrier.controller", ["mealcarrier.services", "mealcarrier.f
 	}
 })
 
-.controller('messaging_controller', function($scope, $timeout, $ionicScrollDelegate) {
+.controller('messaging_controller', ['$scope', '$rootScope', '$state',
+  '$stateParams', 'MockService', '$ionicActionSheet',
+  '$ionicPopup', '$ionicScrollDelegate', '$timeout', '$interval',
+  function($scope, $rootScope, $state, $stateParams, MockService,
+    $ionicActionSheet,
+    $ionicPopup, $ionicScrollDelegate, $timeout, $interval) {
 
-  $scope.hideTime = true;
+    // mock acquiring data via $stateParams
+    $scope.toUser = {
+      _id: '534b8e5aaa5e7afc1b23e69b',
+      pic: 'http://ionicframework.com/img/docs/venkman.jpg',
+      username: 'Venkman'
+    }
 
-  var alternate,
-    isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+    // this could be on $rootScope rather than in $stateParams
+    $scope.user = {
+      _id: '534b8fb2aa5e7afc1b23e69c',
+      pic: 'http://ionicframework.com/img/docs/mcfly.jpg',
+      username: 'Marty'
+    };
 
-  $scope.sendMessage = function() {
-    alternate = !alternate;
+    $scope.input = {
+      message: localStorage['userMessage-' + $scope.toUser._id] || ''
+    };
 
-    var d = new Date();
-  d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+    var messageCheckTimer;
 
-    $scope.messages.push({
-      userId: alternate ? '12345' : '54321',
-      text: $scope.data.message,
-      time: d
+    var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
+    var footerBar; // gets set in $ionicView.enter
+    var scroller;
+    var txtInput; // ^^^
+
+    $scope.$on('$ionicView.enter', function() {
+      console.log('UserMessages $ionicView.enter');
+
+      getMessages();
+      
+      $timeout(function() {
+        footerBar = document.body.querySelector('#userMessagesView .bar-footer');
+        scroller = document.body.querySelector('#userMessagesView .scroll-content');
+        txtInput = angular.element(footerBar.querySelector('textarea'));
+      }, 5000);
+
+      messageCheckTimer = $interval(function() {
+        // here you could check for new messages if your app doesn't use push notifications or user disabled them
+      }, 20000);
     });
 
-    delete $scope.data.message;
-    $ionicScrollDelegate.scrollBottom(true);
+    $scope.$on('$ionicView.leave', function() {
+      console.log('leaving UserMessages view, destroying interval');
+      // Make sure that the interval is destroyed
+      if (angular.isDefined(messageCheckTimer)) {
+        $interval.cancel(messageCheckTimer);
+        messageCheckTimer = undefined;
+      }
+    });
 
-  };
+    $scope.$on('$ionicView.beforeLeave', function() {
+      if (!$scope.input.message || $scope.input.message === '') {
+        localStorage.removeItem('userMessage-' + $scope.toUser._id);
+      }
+    });
 
+    function getMessages() {
+      // the service is mock but you would probably pass the toUser's GUID here
+      MockService.getUserMessages({
+        toUserId: $scope.toUser._id
+      }).then(function(data) {
+        $scope.doneLoading = true;
+        $scope.messages = data.messages;
 
-  $scope.inputUp = function() {
-    if (isIOS) $scope.data.keyboardHeight = 216;
-    $timeout(function() {
-      $ionicScrollDelegate.scrollBottom(true);
-    }, 300);
+        $timeout(function() {
+          viewScroll.scrollBottom();
+        }, 5000);
+      });
+    }
 
-  };
+    $scope.$watch('input.message', function(newValue, oldValue) {
+      console.log('input.message $watch, newValue ' + newValue);
+      if (!newValue) newValue = '';
+      localStorage['userMessage-' + $scope.toUser._id] = newValue;
+    });
 
-  $scope.inputDown = function() {
-    if (isIOS) $scope.data.keyboardHeight = 0;
-    $ionicScrollDelegate.resize();
-  };
+    $scope.sendMessage = function(sendMessageForm) {
+      var message = {
+        toId: $scope.toUser._id,
+        text: $scope.input.message
+      };
 
-  $scope.closeKeyboard = function() {
-    // cordova.plugins.Keyboard.close();
-  };
+      // if you do a web service call this will be needed as well as before the viewScroll calls
+      // you can't see the effect of this in the browser it needs to be used on a real device
+      // for some reason the one time blur event is not firing in the browser but does on devices
+      keepKeyboardOpen();
+      
+      //MockService.sendMessage(message).then(function(data) {
+      $scope.input.message = '';
 
+      message._id = new Date().getTime(); // :~)
+      message.date = new Date();
+      message.username = $scope.user.username;
+      message.userId = $scope.user._id;
+      message.pic = $scope.user.picture;
 
-  $scope.data = {};
-  $scope.myId = '12345';
-  $scope.messages = [];
+      $scope.messages.push(message);
 
-});
+      $timeout(function() {
+        keepKeyboardOpen();
+        viewScroll.scrollBottom(true);
+      }, 5000);
+
+      $timeout(function() {
+        $scope.messages.push(MockService.getMockMessage());
+        keepKeyboardOpen();
+        viewScroll.scrollBottom(true);
+      }, 2000);
+
+      //});
+    };
+    
+    // this keeps the keyboard open on a device only after sending a message, it is non obtrusive
+    function keepKeyboardOpen() {
+      console.log('keepKeyboardOpen');
+      txtInput.one('blur', function() {
+        console.log('textarea blur, focus back on it');
+        txtInput[0].focus();
+      });
+    }
+
+    $scope.onMessageHold = function(e, itemIndex, message) {
+      console.log('onMessageHold');
+      console.log('message: ' + JSON.stringify(message, null, 2));
+      $ionicActionSheet.show({
+        buttons: [{
+          text: 'Copy Text'
+        }, {
+          text: 'Delete Message'
+        }],
+        buttonClicked: function(index) {
+          switch (index) {
+            case 0: // Copy Text
+              //cordova.plugins.clipboard.copy(message.text);
+
+              break;
+            case 1: // Delete
+              // no server side secrets here :~)
+              $scope.messages.splice(itemIndex, 1);
+              $timeout(function() {
+                viewScroll.resize();
+              }, 5000);
+
+              break;
+          }
+          
+          return true;
+        }
+      });
+    };
+
+    // this prob seems weird here but I have reasons for this in my app, secret!
+    $scope.viewProfile = function(msg) {
+      if (msg.userId === $scope.user._id) {
+        // go to your profile
+      } else {
+        // go to other users profile
+      }
+    };
+    
+    // I emit this event from the monospaced.elastic directive, read line 480
+    $scope.$on('taResize', function(e, ta) {
+      console.log('taResize');
+      if (!ta) return;
+      
+      var taHeight = ta[0].offsetHeight;
+      console.log('taHeight: ' + taHeight);
+      
+      if (!footerBar) return;
+      
+      var newFooterHeight = taHeight + 10;
+      newFooterHeight = (newFooterHeight > 44) ? newFooterHeight : 44;
+      
+      footerBar.style.height = newFooterHeight + 'px';
+      scroller.style.bottom = newFooterHeight + 'px'; 
+    });
+
+}])
+;
--- www/js/services.js
+++ www/js/services.js
@@ -96,4 +96,41 @@ angular.module("mealcarrier.services", [])
 		    console.log("all done with request")
 	        return $deferred.promise;
 	    }
-    }]);
+    }])
+
+	.factory('MockService', ['$http', '$q',
+	  function($http, $q) {
+	    var me = {};
+
+	    me.getUserMessages = function(d) {
+	      /*
+	      var endpoint =
+	        'http://www.mocky.io/v2/547cf341501c337f0c9a63fd?callback=JSON_CALLBACK';
+	      return $http.jsonp(endpoint).then(function(response) {
+	        return response.data;
+	      }, function(err) {
+	        console.log('get user messages error, err: ' + JSON.stringify(
+	          err, null, 2));
+	      });
+	      */
+	      var deferred = $q.defer();
+	      
+			 setTimeout(function() {
+	      	deferred.resolve(getMockMessages());
+		    }, 1500);
+	      
+	      return deferred.promise;
+	    };
+
+	    me.getMockMessage = function() {
+	      return {
+	        userId: '534b8e5aaa5e7afc1b23e69b',
+	        date: new Date(),
+	        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+	      };
+	    }
+
+	    return me;
+	  }
+	])
+;
